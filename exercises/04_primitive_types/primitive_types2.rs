71baf52e664bf06148b91a2037361e0bfa8779f2
Taj Macé <taj.mac101@gmail.com>
2026-08-13 21:10:27 +1000

    feat: finished set 4

--- exercises/04_primitive_types/primitive_types2.rs
+++ exercises/04_primitive_types/primitive_types2.rs
@@ -12,12 +12,7 @@ fn main() {
         println!("Neither alphabetic nor numeric!");
     }
 
-    // TODO: Analogous to the example before, declare a variable called `your_character`
-    // below with your favorite character.
-    // Try a letter, try a digit (in single quotes), try a special character, try a character
-    // from a different language than your own, try an emoji 😉
-    // let your_character = '';
-
+    let your_character = 'a';
     if your_character.is_alphabetic() {
         println!("Alphabetical!");
     } else if your_character.is_numeric() {
