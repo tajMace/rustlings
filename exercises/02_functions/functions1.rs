78f58bf571bb2c5753e3a52ed282c1a59222c2c1
Taj Macé <taj.mac101@gmail.com>
2026-08-13 20:13:33 +1000

    feat: finished set 2

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,5 +1,11 @@
 // TODO: Add some function with the name `call_me` without arguments or a return value.
 
+use std::println;
+
 fn main() {
     call_me(); // Don't change this line
+}
+
+fn call_me() {
+    println!("Hey, I just met you...");
 }
